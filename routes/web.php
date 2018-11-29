5e4516879d0c745f7bf9bfb28e1ca5fe68a85405
Unknown <a.micarelli6@gmail.com>
2018-11-29 15:29:07 +0100

    porcodio

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get('/', function () {
     return view('layouts/app');
 });
 
+//Route::get('/', function () {
+////    return view('welcome');
+//});
+
 Route::get('/index', function () {
     return view('index');
 });
